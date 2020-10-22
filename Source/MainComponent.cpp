72ebed85e873f4ef8555a55782037b5d8025ba53
nazar pavliuk <hesab.k18@gmail.com>
2020-10-22 03:01:32 +0300

    add UI components

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -1,13 +1,135 @@
 #include "MainComponent.h"
 
 
-MainComponent::MainComponent()
+MainComponent::MainComponent() :
+	testProgramLabelLabel("testProgramLabelLabel", "Test program with file"),
+	testProgramQueryLabel("testProgramQueryLabel", "Query list:"),
+	testProgramOutputLabel("testProgramOutputLabel", "Output:"),
+	inputDataLabel("inputDataLabel", "Input data:"),
+	getTextButton("Get"),
+	testProgramTestTextButton("Test"),
+	testProgramSelectFileTextButton("SelectFile"),
+	queryLabel("queryLabel", "Query:"),
+	outputLabel("outputLabel", "Output data:")
 {
-	setSize(600, 400);
+	size_t windowW = 1280;
+	size_t windowH = 720;
+	setSize(windowW, windowH);
+	inputDataLabel.setTopLeftPosition(15, 5);
+	inputDataLabel.setSize(300, 40);
+	inputDataLabel.setFont(juce::Font(24.f));
+	addAndMakeVisible(inputDataLabel);
+	//
+	inputDataTextEditor.setTopLeftPosition(10, 40);
+	inputDataTextEditor.setSize(800, 400);
+	inputDataTextEditor.setMultiLine(true);
+	inputDataTextEditor.setReturnKeyStartsNewLine(true);
+	inputDataTextEditor.setTabKeyUsedAsCharacter(true);
+	inputDataTextEditor.setFont(juce::Font(18.f));
+	addAndMakeVisible(inputDataTextEditor);
+	//
+	queryLabel.setTopLeftPosition(15, 450);
+	queryLabel.setSize(300, 40);
+	queryLabel.setFont(juce::Font(24.f));
+	addAndMakeVisible(queryLabel);
+	//
+	queryTextEditor.setTopLeftPosition(10, 490);
+	queryTextEditor.setSize(600, 40);
+	queryTextEditor.setTabKeyUsedAsCharacter(true);
+	queryTextEditor.setFont(juce::Font(18.f));
+	queryTextEditor.setJustification(juce::Justification::centredLeft);
+	addAndMakeVisible(queryTextEditor);
+	//
+	getTextButton.setTopLeftPosition(615, 490);
+	getTextButton.setColour(juce::TextButton::ColourIds::buttonColourId, juce::Colours::red);
+	getTextButton.setSize(185, 40);
+	getTextButton.onClick = [&]() {
+		std::vector<std::shared_ptr<BaseZGMLOperator>> useSymbols;
+		useSymbols.push_back(std::make_shared<DotOperator>());
+		useSymbols.push_back(std::make_shared<TildeOperator>());
+		std::shared_ptr<BaseZGMLOperator> baseSymbol = std::make_shared<DotOperator>();
+		Parser p;
+		outputTextEditor.setText(p.parse(inputDataTextEditor.getText().toStdString(), queryTextEditor.getText().toStdString(), useSymbols, baseSymbol));
+	};
+	addAndMakeVisible(getTextButton);
+	//
+	outputLabel.setTopLeftPosition(15, 540);
+	outputLabel.setSize(300, 40);
+	outputLabel.setFont(juce::Font(24.f));
+	addAndMakeVisible(outputLabel);
+	//
+	outputTextEditor.setTopLeftPosition(10, 580);
+	outputTextEditor.setSize(800, 40);
+	outputTextEditor.setTabKeyUsedAsCharacter(true);
+	outputTextEditor.setFont(juce::Font(18.f));
+	outputTextEditor.setJustification(juce::Justification::centredLeft);
+	outputTextEditor.setMultiLine(true);
+	outputTextEditor.setReturnKeyStartsNewLine(true);
+	outputTextEditor.setTabKeyUsedAsCharacter(true);
+	outputTextEditor.setEnabled(false);
+	addAndMakeVisible(outputTextEditor);
 
 
 
-	/*std::string inputData("<tag><tag2></tag2><tag1><tag1 arg = \"is work\"></tag1></tag1></tag>");
+
+	//////////////#########################
+	testProgramLabelLabel.setTopLeftPosition(windowW - 340, 30);
+	testProgramLabelLabel.setSize(300, 40);
+	testProgramLabelLabel.setFont(juce::Font(32.f));
+	testProgramLabelLabel.setColour(juce::Label::ColourIds::backgroundColourId, juce::Colours::red);
+	addAndMakeVisible(testProgramLabelLabel);
+	//
+	testProgramSelectFileTextButton.setTopLeftPosition(windowW - 250, 90);
+	testProgramSelectFileTextButton.setSize(130, 50);
+	testProgramSelectFileTextButton.onClick = [&]() {
+		/*fileExplorer.browseForFileToOpen();
+		juce::String name = fileExplorer.getResult().getFullPathName();*/
+	};
+	addAndMakeVisible(testProgramSelectFileTextButton);
+	//
+	testProgramQueryLabel.setTopLeftPosition(windowW - 340, 140);
+	testProgramQueryLabel.setSize(200, 40);
+	testProgramQueryLabel.setFont(juce::Font(24.f));
+	addAndMakeVisible(testProgramQueryLabel);
+	//
+	testProgramQueryTextEditor.setTopLeftPosition(windowW - 340, 180);
+	testProgramQueryTextEditor.setSize(330, 140);
+	testProgramQueryTextEditor.setMultiLine(true);
+	testProgramQueryTextEditor.setReturnKeyStartsNewLine(true);
+	testProgramQueryTextEditor.setTabKeyUsedAsCharacter(true);
+	testProgramQueryTextEditor.setFont(juce::Font(18.f));
+	testProgramQueryTextEditor.setEnabled(false);
+	addAndMakeVisible(testProgramQueryTextEditor);
+	//
+	testProgramOutputLabel.setTopLeftPosition(windowW - 340, 330);
+	testProgramOutputLabel.setSize(200, 40);
+	testProgramOutputLabel.setFont(juce::Font(24.f));
+	addAndMakeVisible(testProgramOutputLabel);
+	//
+	testProgramOutputTextEditor.setTopLeftPosition(windowW - 340, 380);
+	testProgramOutputTextEditor.setSize(330, 140);
+	testProgramOutputTextEditor.setMultiLine(true);
+	testProgramOutputTextEditor.setReturnKeyStartsNewLine(true);
+	testProgramOutputTextEditor.setTabKeyUsedAsCharacter(true);
+	testProgramOutputTextEditor.setFont(juce::Font(18.f));
+	testProgramOutputTextEditor.setEnabled(false);
+	addAndMakeVisible(testProgramOutputTextEditor);
+	//
+	testProgramTestTextButton.setTopLeftPosition(windowW - 250, 530);
+	testProgramTestTextButton.setColour(juce::TextButton::ColourIds::buttonColourId, juce::Colours::red);
+	testProgramTestTextButton.setSize(130, 50);
+	testProgramTestTextButton.onClick = [&]() {
+		/*fileExplorer.browseForFileToOpen();
+		juce::String name = fileExplorer.getResult().getFullPathName();*/
+	};
+	addAndMakeVisible(testProgramTestTextButton);
+	//
+
+
+
+
+
+	/*std::string inputData_("<tag><tag2>\n</tag2><tag1><tag1 arg = \"is work\"></tag1></tag1></tag>");
 	std::string query("tag.tag1.tag1~arg");
 	std::vector<std::shared_ptr<BaseZGMLOperator>> useSymbols;
 	useSymbols.push_back(std::make_shared<DotOperator>());
@@ -15,7 +137,7 @@ MainComponent::MainComponent()
 	std::shared_ptr<BaseZGMLOperator> baseSymbol = std::make_shared<DotOperator>();
 	Parser p;
 	std::chrono::steady_clock::time_point start = std::chrono::high_resolution_clock::now();
-	std::string re =p.parse(inputData, query, useSymbols, baseSymbol);*/
+	std::string re =p.parse(inputData_, query, useSymbols, baseSymbol);*/
 }
 
 MainComponent::~MainComponent()
@@ -27,11 +149,12 @@ void MainComponent::paint(juce::Graphics& g)
 	// (Our component is opaque, so we must completely fill the background with a solid colour)
 	g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
 
-	g.setFont(juce::Font(16.0f));
-	g.setColour(juce::Colours::white);
-	g.drawText("Hello World!", getLocalBounds(), juce::Justification::centred, true);
+	g.setFont(juce::Font(26.f));
+	g.setColour(juce::Colours::grey);
+	g.drawText("Zagrava test task", getLocalBounds(), juce::Justification::centredTop, true);
 }
 
 void MainComponent::resized()
 {
+
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -9,12 +9,24 @@ class MainComponent  : public juce::Component
 public:
     MainComponent();
     ~MainComponent() override;
-
     void paint (juce::Graphics&) override;
     void resized() override;
-
 private:
-    juce::TextButton res;
+    juce::TextEditor inputDataTextEditor;
+    juce::TextEditor queryTextEditor;
+    juce::TextButton getTextButton;
+    juce::Label inputDataLabel;
+    juce::Label queryLabel;
+    juce::Label outputLabel;
+    juce::TextEditor outputTextEditor;
+    /////////////////////
+    juce::TextButton testProgramSelectFileTextButton;
+    juce::TextButton testProgramTestTextButton;
+    juce::Label testProgramLabelLabel;
+    juce::Label testProgramQueryLabel;
+    juce::TextEditor testProgramQueryTextEditor;
+    juce::Label testProgramOutputLabel;
+    juce::TextEditor testProgramOutputTextEditor;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
